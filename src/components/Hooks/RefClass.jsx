2fbfacd274f9678ffbf3e6703533256d36399c6d
leeraquel <thisisdayounglee@gmail.com>
2024-04-12 15:37:45 +0900

    study: class형으로 create ref 사용하기

--- src/components/Hooks/RefClass.jsx
+++ src/components/Hooks/RefClass.jsx
@@ -1,10 +1,22 @@
-import { Component } from "react";
+import { Component, createRef } from "react";
 
 export default class RefClass extends Component {
+  input = createRef();
+
   render() {
     return (
       <>
         <div>클래스형 useRef 기능 쓰기</div>
+        <input type="text" ref={this.input} />
+        <button
+          type="button"
+          onClick={() => {
+            this.input.current.focus();
+          }}
+        >
+          on focus btn
+        </button>
+        <br></br>
       </>
     );
   }
